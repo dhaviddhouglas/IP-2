28d64dd7582f4f623eb0d4d0cbdc88cdfa8454cd
Dhavid Messias <dhaviddhouglas@users.noreply.github.com>
2018-06-07 23:48:36 -0300

    Sistema de Vendas

---
+++ Vendas/src/Pessoa_juridica.java
@@ -0,0 +1,14 @@
+
+public class Pessoa_juridica extends Pessoa {
+
+	private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+}
---
+++ Vendas/src/Venda.java
@@ -0,0 +1,10 @@
+
+public class Venda {
+	
+	Produto produto;
+	Vendedor vendedor;
+	int quantidade;
+	Cliente cliente;
+	String dataVenda;
+
+}
